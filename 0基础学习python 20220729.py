c442ff883fa73011b4072c244661407332c967f7
Elsa <togethersuc@gmail.com>
2022-08-01 12:26:14 +0800

    Adda logic if guess==1 then print hellow word

--- 0基础学习python 20220729.py
+++ 0基础学习python 20220729.py
@@ -4,6 +4,8 @@ guess=int(8)
 if guess==8:
     print("我草，你是小甲鱼心里的蛔虫吗?!")
     print("哼，猜中了也没有奖励!")
+elif guess==1:
+    print("Hello Word")
 else:
     print("猜错啦，小甲鱼现在心里想的是8!")
 print("游戏结束,不玩啦^_^")
